7fc35accd8dbe5b16124c0088e04c831774712e0
sara1485l <saraahmed0855l@gmail.com>
2025-09-17 12:06:56 +0300

    products

--- E-commerceProject/index.js
+++ E-commerceProject/index.js
@@ -11,20 +11,18 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
+// const authRoutes = require("./routes/authRoutes");
+// const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
-const cartRoutes = require("./routes/cartRoutes");
-const buyRoutes = require("./routes/buyRoutes");
-const couponRoutes = require("./routes/couponRoutes");
+// const cartRoutes = require("./routes/cartRoutes");
+// const buyRoutes = require("./routes/buyRoutes");
 
-// Use Routes
-app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
+// // Use Routes
+// app.use("/auth", authRoutes);
+// app.use("/user", userRoutes);
 app.use("/products", productRoutes);
-app.use("/shopping-cart", cartRoutes);
-app.use("/buy", buyRoutes);
-app.use("/coupon", couponRoutes);
+// app.use("/shopping-cart", cartRoutes);
+// app.use("/buy", buyRoutes);
 
 // Connect DB
 connectDB();
---
+++ E-commerceProject/routes/productRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllProducts,
+  getProduct,
+  addProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/productController");
+
+// Public
+router.get("/all-product", getAllProducts);
+router.get("/:product", getProduct);
+
+// middleware auth 
+router.post("/add-product", addProduct);
+router.delete("/delete-product", deleteProduct);
+router.put("/change-data", updateProduct);
+
+module.exports = router;

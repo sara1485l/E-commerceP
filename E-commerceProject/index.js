27a80ee8056340cb78c7c800c70dd7bd17fb056d
sara1485l <saraahmed0855l@gmail.com>
2025-09-17 11:37:29 +0300

    .

---
+++ E-commerceProject/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const buyRoutes = require("./routes/buyRoutes");
+const couponRoutes = require("./routes/couponRoutes");
+
+// Use Routes
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
+app.use("/products", productRoutes);
+app.use("/shopping-cart", cartRoutes);
+app.use("/buy", buyRoutes);
+app.use("/coupon", couponRoutes);
+
+// Connect DB
+connectDB();
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
